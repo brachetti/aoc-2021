126a24acd2510eeeed78048e71148990925418ca
Ronald Brachetti <ronald.brachetti@gmail.com>
2021-12-18 13:02:35 +0100

    restructure, but still too slow

--- day 06/rust/problem 01/src/lib.rs
+++ day 06/rust/problem 01/src/lib.rs
@@ -1,69 +1,23 @@
 use std::fmt;
-use std::num::ParseIntError;
 use std::str::FromStr;
 
 use log::{info, debug};
 
 const FISH_RESTART_TIMER: u8 = 6;
 const FISH_SPAWN_TIMER: u8 = 8;
-const RUN_FOR_DAYS: u16 = 80;
-
-#[derive(Copy, Clone, Debug)]
-struct Fish {
-    timer: u8,
-}
-
-
-impl Fish {
-    fn new(timer: u8) -> Self {
-        Fish { timer }
-    }
-
-    fn spawn_new() -> Self {
-        Fish::new(FISH_SPAWN_TIMER)
-    }
-
-    fn spawns_new_fish(&self) -> bool {
-        self.timer == 0
-    }
-
-    fn age(&mut self) {
-        if self.spawns_new_fish() {
-            self.timer = FISH_RESTART_TIMER;
-            return
-        }
-        self.timer = self.timer - 1;
-    }
-}
-
-impl fmt::Display for Fish {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.timer)
-    }
-}
-
-impl FromStr for Fish {
-    type Err = ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        debug!("Fish from string {:?}", s);
-        let value = s.parse::<u8>()?;
-
-        Ok(Fish::new(value))
-    }
-}
+const RUN_FOR_DAYS: u16 = 256;
 
 #[derive(Clone, Debug)]
 pub struct Simulation {
     days: u16,
-    fishes: Vec<Fish>,
+    fishes: Vec<u8>,
 }
 
 impl Simulation {
     pub fn new(input: String) -> Self {
         Simulation {
             days: 0,
-            fishes: input.split_terminator(",").map(str::trim).map(Fish::from_str).map(|res| res.unwrap()).collect(),
+            fishes: input.split_terminator(",").map(str::trim).map(u8::from_str).map(|res| res.unwrap()).collect(),
         }
     }
 
@@ -73,6 +27,7 @@ impl Simulation {
             self.days = day;
             debug!("{}", self);
             self.age_one_day();
+            info!("Day {}", day);
         }
         self.days = RUN_FOR_DAYS;
         debug!("{}", self);
@@ -85,18 +40,34 @@ impl Simulation {
     }
 
     fn age_one_day(&mut self) {
-        let mut new_fishes: Vec<Fish> = Vec::new();
-        for mut fish in self.fishes.iter_mut() {
-            if fish.spawns_new_fish() {
-                new_fishes.push(Fish::spawn_new());
+        let mut new_fishes: Vec<u8> = Vec::new();
+        self.fishes = self.fishes.iter().map(|fish| {
+            if self.spawns_new_fish(fish.clone()) {
+                new_fishes.push(self.spawn_new());
             }
-            fish.age();
-        }
+            self.age(*fish)
+        }).collect();
         self.fishes.append(&mut new_fishes);
     }
 
+    fn spawn_new(&self) -> u8 {
+        FISH_SPAWN_TIMER
+    }
+
+    fn spawns_new_fish(&self, num: u8) -> bool {
+        num == 0
+    }
+
+    fn age(&self, fish: u8) -> u8{
+        if self.spawns_new_fish(fish) {
+            FISH_RESTART_TIMER
+        } else {
+            fish - 1
+        }
+    }
+
     fn fmt_fishes(&self) -> String {
-        let res: Vec<String> = self.fishes.iter().map(Fish::to_string).collect();
+        let res: Vec<String> = self.fishes.iter().map(u8::to_string).collect();
         res.join(",")
     }
 }
