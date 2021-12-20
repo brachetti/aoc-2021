a166b1f98b969e7c1ea1ce7126492bb34c57627b
Ronald Brachetti <ronald.brachetti@gmail.com>
2021-12-20 19:42:34 +0100

    wip

--- day 09/rust/problem 02/src/lib.rs
+++ day 09/rust/problem 02/src/lib.rs
@@ -1,12 +1,13 @@
 use std::{
     cmp::{max, min},
+    collections::{HashMap, HashSet},
+    fmt,
     result::Result,
-    fmt
 };
 
-use log::{info, debug};
+use log::{debug, info};
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 struct Point {
     x: usize,
     y: usize,
@@ -27,7 +28,7 @@ impl Point {
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 struct Location {
     value: usize,
     is_lowest: bool,
@@ -55,7 +56,7 @@ impl Location {
         }
     }
 
-    fn find_neighbours(&self, list: &Vec<Location>) -> Vec<usize> {
+    fn find_neighbours(&self, list: Vec<&Location>) -> Vec<usize> {
         let mut neighbours = Vec::new();
         for location in list {
             if location.is_neighbour_of(&self.point) {
@@ -68,14 +69,31 @@ impl Location {
         neighbours.iter().map(|location| location.value).collect()
     }
 
+    fn find_basin_neighbours<'a>(&self, list: Vec<&'a Location>) -> Vec<&'a Location> {
+        let mut neighbours = Vec::new();
+        for location in list {
+            if self.is_neighbour_in_basin_of(location) {
+                neighbours.push(location);
+            }
+            if neighbours.len() == 4 {
+                break;
+            }
+        }
+        neighbours
+    }
+
     fn is_neighbour_of(&self, point: &Point) -> bool {
         self.point.is_neighbour_of(point)
+    }
+
+    fn is_neighbour_in_basin_of(&self, other: &Location) -> bool {
+        other.value < 9 && other.is_neighbour_of(&self.point)
     }
 
     fn check_lowest(value: usize, neighbour_values: Vec<usize>) -> bool {
         for other in &neighbour_values {
             if value >= *other {
-                return false
+                return false;
             }
         }
         true
@@ -92,23 +110,23 @@ impl Location {
 
 impl fmt::Display for Location {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if self.is_lowest {
-            write!(f, "{}", self.value)
-        } else {
+        if self.value == 9 {
             write!(f, " ")
+        } else {
+            write!(f, "{}", self.value)
         }
     }
 }
 
 #[derive(Clone, Debug)]
-pub struct Simulation {
-    locations: Vec<Location>,
+pub struct Simulation<'a> {
+    locations: Vec<&'a Location>,
     columns: usize,
 }
 
-impl Simulation {
+impl Simulation<'_> {
     pub fn new(original_input: String) -> Self {
-        let mut locations = Vec::new();
+        let mut locations: Vec<&Location> = Vec::new();
         let mut x = 0;
         let mut y = 0;
         let mut columns = 0;
@@ -116,7 +134,8 @@ impl Simulation {
         for line in original_input.lines() {
             for val in line.chars() {
                 let value: u32 = val.to_digit(10).unwrap();
-                locations.push(Location::new(value as usize, x, y));
+                let location = Location::new(value as usize, x, y);
+                locations.push(location);
                 x += 1;
             }
             y += 1;
@@ -127,18 +146,54 @@ impl Simulation {
     }
 
     fn init_locations(&mut self) {
-        self.locations = self.locations
+        self.locations = self
+            .locations
             .iter()
             .map(|l| {
-                let values = l.find_neighbours(&self.locations);
-                Location::with_values(l, values)
+                let values = l.find_neighbours(self.locations);
+                &Location::with_values(l, values)
             })
             .collect()
     }
 
     fn get_risk_levels(&self) {
-        let sum: usize = self.locations.iter().filter(|l| l.is_lowest()).map(|l| l.risk_level()).sum();
+        let sum: usize = self
+            .locations
+            .iter()
+            .filter(|l| l.is_lowest())
+            .map(|l| l.risk_level())
+            .sum();
         info!("Sum of risk levels is {}", sum);
+    }
+
+    fn find_basins(&self) {
+        let mut basins: HashMap<Point, HashSet<&Location>> = HashMap::new();
+        self.locations
+            .iter()
+            .filter(|l| l.is_lowest())
+            .for_each(|l| {
+                let basin = self.add_to_basin(HashSet::new(), vec![l]);
+                basins.insert(l.point, basin);
+            });
+    }
+
+    /**
+     * Iteratively checks all locations for neighbours, that would be part of the basin.
+     */
+    fn add_to_basin<'a>(
+        &self,
+        mut basin: HashSet<&'a Location>,
+        to_check: Vec<&'a Location>,
+    ) -> HashSet<&'a Location> {
+        let mut additional: Vec<&Location> = Vec::new();
+        if to_check.is_empty() {
+            return basin;
+        }
+        for location in to_check {
+            basin.insert(location);
+            let l_neighbours = location.find_basin_neighbours(self.locations);
+        }
+        self.add_to_basin(basin, additional)
     }
 
     pub fn run(&mut self) -> Result<(), ()> {
@@ -146,23 +201,24 @@ impl Simulation {
         self.init_locations();
         debug!("{}", self);
         self.get_risk_levels();
+        self.find_basins();
         info!("Done running Simulation");
         Ok(())
     }
 }
 
-impl fmt::Display for Simulation {
+impl fmt::Display for Simulation<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f);
+        let mut o = String::from("\n");
         let mut column = 0;
         for location in &self.locations {
-            write!(f, "{}", location);
+            o.push_str(&location.to_string());
             column += 1;
             if column == self.columns {
-                writeln!(f);
+                o.push_str("\n");
                 column = 0;
             }
         }
-        writeln!(f)
+        writeln!(f, "{}", o)
     }
 }
