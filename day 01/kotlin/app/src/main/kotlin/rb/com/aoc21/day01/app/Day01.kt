b7c108ac0754fd4c7cd207f88cd3e68a594eb9d8
Ronald Brachetti <ronald.brachetti@gmail.com>
2021-12-25 19:49:20 +0100

    solving problem 2 for day 1 in kotlin :)

--- day 01/kotlin/app/src/main/kotlin/rb/com/aoc21/day01/app/Day01.kt
+++ day 01/kotlin/app/src/main/kotlin/rb/com/aoc21/day01/app/Day01.kt
@@ -14,11 +14,27 @@ class Day01(private val inputs: List<Int>) {
 
         when (problem) {
             1 -> problem01()
+            2 -> problem02()
         }
+    }
+
+    private fun problem02() {
+        logger.info { "Count the number of times the sum of measurements in this sliding window increases from the previous sum." }
+        val windows = inputs.windowed(3)
+        logger.debug { "Windows: ${windows.subList(0, 4)}..." }
+        val windowSums = windows.map { it.sumOf { it } }
+        logger.debug { "Window Sums: ${windowSums.subList(0, 4)}..." }
+        val increaseCount = increaseCountFor(windowSums)
+        logger.info { "Increases: $increaseCount" }
     }
 
     private fun problem01() {
         logger.info { "Count the number of times a depth measurement increases from the previous measurement." }
+        val increaseCount = increaseCountFor(inputs)
+        logger.info { "Increases: $increaseCount" }
+    }
+
+    private infix fun increaseCountFor(inputs: List<Int>): Int {
         var increaseCount = 0
         logger.debug { "${inputs.first()} -> init" }
         inputs.forEachIndexed { index, elem ->
@@ -33,6 +49,6 @@ class Day01(private val inputs: List<Int>) {
                 logger.debug { "$elem" }
             }
         }
-        logger.info { "Increases: $increaseCount" }
+        return increaseCount
     }
 }
